8950d8675c2fce6aaae6136d71c7fda76c56c50f
Kagoharo <40521742+Kagoharo@users.noreply.github.com>
2022-03-16 16:11:16 +0300

    Update views.py

--- Quiz/quizlet/views.py
+++ Quiz/quizlet/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.db import models
-from django.shortcuts import render, redirect
-from django.utils import timezone
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
-from .models import *
+from .models import Quiz, Category, Question
 
 
 class QuizListView(ListView):
+    """
+    Вид для списка опросов.
+    """
+    
     model = Quiz
     template_name = 'quiz_list.html'
     context_object_name = 'quizzes'
@@ -16,6 +15,10 @@ class QuizListView(ListView):
 
 
 class CategoryListView(ListView):
+    """
+    Вид для списка категорий.
+    """
+    
     model = Category
     template_name = 'category_list.html'
     context_object_name = 'categories'
@@ -23,6 +26,10 @@ class CategoryListView(ListView):
 
 
 class ShowQuiz(DetailView):
+    """
+    Вид для вопросов связанных с категорией.
+    """
+    
     model = Question
     template_name = 'question_list.html'
     context_object_name = 'questions'
