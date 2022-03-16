241b77c2b50a11326e8ed97af3bd83868a43feaa
Kagoharo <kagoharo@mail.ru>
2022-03-16 22:50:44 +0300

    commit

--- Quiz/quizlet/admin.py
+++ Quiz/quizlet/admin.py
@@ -1,26 +1,42 @@
 from django.contrib import admin
-from .models import *
+from .models import Quiz, Category, Question, Answer
 
 
 class AdminQuiz(admin.ModelAdmin):
+    """
+    Модель опросов для админа.
+    """
+
     list_display = ('id', 'quiz_name', 'created_at')
     list_display_links = ('id', 'quiz_name')
     search_fields = ('quiz_name',)
 
 
 class AdminCategory(admin.ModelAdmin):
+    """
+    Модель категорий для админа.
+    """
+
     list_display = ('id', 'category_name', 'quiz_id', 'created_at')
     list_display_links = ('id', 'category_name')
     search_fields = ('category_name', 'quiz_id')
 
 
 class AdminQuestion(admin.ModelAdmin):
+    """
+    Модель вопросов для админа.
+    """
+
     list_display = ('id', 'question', 'quiz_id', 'category_id', 'created_at')
     list_display_links = ('id', 'question')
     search_fields = ('id', 'question', 'quiz_id', 'category_id')
 
 
 class AdminAnswer(admin.ModelAdmin):
+    """
+    Модель ответов для админа.
+    """
+
     list_display = ('id', 'answer', 'is_correct', 'question_id', 'created_at')
     list_display_links = ('id', 'answer')
     search_fields = ('answer', 'is_correct', 'question_id')
