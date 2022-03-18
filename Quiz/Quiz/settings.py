fc58319d6968dc27b960023b3b396c5a3b8adfd5
Kagoharo <40521742+Kagoharo@users.noreply.github.com>
2022-03-18 14:07:13 +0300

    Update settings.py

--- Quiz/Quiz/settings.py
+++ Quiz/Quiz/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=d)+b3+w9xesvs)tie-q4*7(_$csa$w1#^$9@d+q5qa!lu3mh4'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
