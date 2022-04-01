ac298b25d18d13dd291a00a1436179c66605e87f
Kagoharo <kagoharo@mail.ru>
2022-04-01 19:06:09 +0300

    Методы для создания и изменения вынесены в отдельный файл crud_methods.py Небольшие правки кода(удаление ненужных импортов, изменение наименования методов)

--- Quiz/quizlet/views.py
+++ Quiz/quizlet/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
 from .models import Quiz, Category
-from .forms import CategoryForm, QuizForm
 
 
 class ViewsMixin:
@@ -34,37 +32,6 @@ class CategoryListView(ViewsMixin, ListView):
     context_object_name = 'categories'
 
 
-def category_create_view(request):
-    """
-    Вид списка категорий.
-    """
-    form = CategoryForm()
-    context = {"form": form}
-    if request.method == "POST":
-        form = CategoryForm(request.POST)
-        context['form'] = form
-        if form.is_valid():
-            form.save()
-        return redirect('http://127.0.0.1:8000/quizzes')
-    return render(request, "category_form.html", context=context)
-
-
-def category_update_view(request, pk):
-    """
-    Вид списка категорий.
-    """
-    category = Category.objects.get(id=pk)
-    form = CategoryForm(instance=category)
-    context = {"form": form}
-    if request.method == "POST":
-        form = CategoryForm(request.POST, instance=category)
-        context['form'] = form
-        if form.is_valid():
-            form.save()
-        return redirect('http://127.0.0.1:8000/quizzes')
-    return render(request, "category_form.html", context=context)
-
-
 class CategoryDetailView(ViewsMixin, DetailView):
     """
     Вид категории.
@@ -76,39 +43,6 @@ class CategoryDetailView(ViewsMixin, DetailView):
 
     def get_title(self):
         return self.get_object().category_name + " опросы"
-
-
-def quiz_create_view(request, pk):
-    """
-    Вид списка категорий.
-    """
-    form = QuizForm()
-    pk = pk
-    url = 'http://127.0.0.1:8000/quizzes/category/' + str(pk)
-    context = {"form": form}
-    if request.method == "POST":
-        form = QuizForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect(url)
-    return render(request, "quiz_form.html", context=context)
-
-
-def quiz_update_view(request, pk):
-    """
-    Вид списка категорий.
-    """
-    url = 'http://127.0.0.1:8000/quizzes/category/' + str(pk)
-    quiz = Quiz.objects.get(id=pk)
-    form = QuizForm(instance=quiz)
-    context = {"form": form}
-    if request.method == "POST":
-        form = QuizForm(request.POST, instance=quiz)
-        context['form'] = form
-        if form.is_valid():
-            form.save()
-        return redirect(url)
-    return render(request, "quiz_form.html", context=context)
 
 
 class QuizDetailView(ViewsMixin, DetailView):
